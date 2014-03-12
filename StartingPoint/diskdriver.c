f8ad52b02224b46ca3fa16619171f148842bf81f
Adam Kurkiewicz <adam@kurkiewicz.pl>
2014-03-12 01:37:05 +0000

    finished and done.

--- StartingPoint/diskdriver.c
+++ StartingPoint/diskdriver.c
@@ -24,6 +24,38 @@
 #include "voucher.h"
 #include "freesectordescriptorstore_full.h"
 #include "sectordescriptorcreator.h"
+#include "BoundedBuffer.h"
+#include "unistd.h"
+#include "pthread.h"
+
+
+// let Voucher be a pointer type on struct Voucher_s 
+struct Voucher_s{
+    // 1 if read request, 0 if write request
+    int read;
+
+    // signalled when request written to or read from the drive
+    pthread_cond_t condition;
+    pthread_mutex_t m;
+
+    // -1 if still waiting, 0 if failure 1 if success
+    int successful;
+    
+    // hold on to the sd from request, we have to fill it or return it to store.
+    SectorDescriptor* sd; 
+};
+
+//allocates and initialised the voucher
+struct Voucher_s* malloc_voucher(){
+    struct Voucher_s* returned = (struct Voucher_s*) malloc(sizeof(struct Voucher_s));
+    returned->read = -1;
+    pthread_mutex_init(&(returned->m), NULL);
+    pthread_cond_init(&(returned->condition), NULL);
+    returned->successful = -1;
+    returned->sd = (SectorDescriptor*) malloc(sizeof(SectorDescriptor));
+    return returned;
+}
+
 /*
  * the following calls must be implemented by the students
  */
@@ -39,17 +71,52 @@
  *             from the memory provided in the two previous arguments
  */
 
-static DiskDevice my_disk;
+//useful globals.
+static DiskDevice DISK;
+static const int BUFFER_SIZE = 100;
+static BoundedBuffer WRITE_BUFFER;
+static BoundedBuffer READ_BUFFER;
+static pthread_t READER_ID;
+static pthread_t WRITER_ID;
+static FreeSectorDescriptorStore STORE;
+
+void reader_thread(){
+    while(1){
+        struct Voucher_s* v = (struct Voucher_s*) (Voucher) blockingReadBB(READ_BUFFER);
+        int success = -1; 
+        if(v->read == 1){
+            v->successful = read_sector(DISK, v->sd);
+            pthread_cond_broadcast(&(v->condition));
+        } else if (v->read == 0) {
+            v->successful = write_sector(DISK, v->sd);
+            pthread_cond_broadcast(&(v->condition));
+            //blocking_put_sd(STORE, *(v->sd));
+        } else {
+            printf("error in reader_thread\n");
+            exit(1);
+        }
+    }
+}
 
 void init_disk_driver(DiskDevice dd, void *mem_start, unsigned long mem_length,
         FreeSectorDescriptorStore *fsds_ptr){
     
-    FreeSectorDescriptorStore store = create_fsds();
-    create_free_sector_descriptors(store, mem_start, mem_length);
-    *fsds_ptr = store;
-    //printf("hello");
-    //printf("%d", sizeof(store));
-    my_disk = dd;
+    STORE = create_fsds();
+    create_free_sector_descriptors(STORE, mem_start, mem_length);
+    *fsds_ptr = STORE;
+    DISK = dd;
+    WRITE_BUFFER = createBB(BUFFER_SIZE);
+    READ_BUFFER = createBB(BUFFER_SIZE);
+    
+    
+    pthread_create(&READER_ID, NULL, (void*(*)(void*)) &reader_thread, NULL);
+    //sleep(1);
+    
+    //wait_on_cond();
+
+    //pthread_join(id0, NULL);
+    //disk_writer
+    
 }
 
 /*
@@ -63,11 +130,20 @@ void init_disk_driver(DiskDevice dd, void *mem_start, unsigned long mem_length,
  * returned that is required to determine the success/failure of the write
  */
 void blocking_write_sector(SectorDescriptor sd, Voucher *v){
-    printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-    sleep(1);
-    printf("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+    struct Voucher_s* voucher = malloc_voucher();
+    *(voucher->sd) = sd;
+    voucher->read = 0;
+    *v = (Voucher) voucher;
+    blockingWriteBB(READ_BUFFER, voucher);
 }
-int nonblocking_write_sector(SectorDescriptor sd, Voucher *v){return 0;}
+int nonblocking_write_sector(SectorDescriptor sd, Voucher *v){
+    struct Voucher_s* voucher = malloc_voucher();
+    *(voucher->sd) = sd;
+    voucher->read = 0;
+    *v = (Voucher) voucher;
+    return nonblockingWriteBB(READ_BUFFER, voucher);
+
+}
 
 /*
  * the following calls are used to initiate the read of a sector from the disk
@@ -79,8 +155,20 @@ int nonblocking_write_sector(SectorDescriptor sd, Voucher *v){return 0;}
  * for successful nonblocking call and for the blocking call, a voucher is
  * returned that is required to collect the sector after the read completes.
  */
-void blocking_read_sector(SectorDescriptor sd, Voucher *v){}
-int nonblocking_read_sector(SectorDescriptor sd, Voucher *v){return 0;}
+void blocking_read_sector(SectorDescriptor sd, Voucher *v){
+    struct Voucher_s* voucher = malloc_voucher();
+    *(voucher->sd) = sd;
+    voucher->read = 1;
+    *v = (Voucher) voucher;
+    blockingWriteBB(READ_BUFFER, voucher);
+}
+int nonblocking_read_sector(SectorDescriptor sd, Voucher *v){
+    struct Voucher_s* voucher = malloc_voucher();
+    *(voucher->sd) = sd;
+    voucher->read = 1;
+    *v = (Voucher) voucher;
+    return nonblockingWriteBB(READ_BUFFER, voucher);
+}
 
 /*
  * the following call is used to retrieve the status of the read or write
@@ -88,5 +176,25 @@ int nonblocking_read_sector(SectorDescriptor sd, Voucher *v){return 0;}
  * the calling application is blocked until the read/write has completed
  * if a successful read, the associated SectorDescriptor is returned in sd
  */
-int redeem_voucher(Voucher v, SectorDescriptor *sd){return 0;}
+//int redeem_voucher(Voucher v, SectorDescriptor *sd){return 0;}
+
+int redeem_voucher(Voucher v, SectorDescriptor *sd){
+    struct Voucher_s* voucher = (struct Voucher_s*) v;
+    pthread_mutex_lock(&(voucher->m));
+    while(voucher->successful == -1){
+        pthread_cond_wait(&(voucher->condition), &(voucher->m));
+    }
+    pthread_mutex_unlock(&(voucher->m));
+    if (voucher->successful == 1 && voucher->read == 1){
+        *sd = *(voucher->sd);
+    }else {
+        blocking_put_sd(STORE, *(voucher->sd));
+    }
+    int cache_success = voucher->successful;
+    free(voucher->sd);
+    pthread_cond_destroy(&(voucher->condition));
+    pthread_mutex_destroy(&(voucher->m));
+    free(voucher);
+    return cache_success;
+}
 
